ac5b3b8f48f1c1af90ab996fd3100568faed13ed
Matias Costilla <matiascostilla96@gmail.com>
2022-03-09 12:26:46 -0300

    Cambios menores

---
+++ src/HolaMatias.java
@@ -0,0 +1,16 @@
+public class HolaMatias {
+    public static void main(String[] args) {
+        String saludar = "Hola Mundo desde Java";
+        System.out.println(saludar);
+        System.out.println("saludar = " + saludar);
+        int numero = 10;
+        System.out.println("numero = " + numero);
+        
+        String nombre;
+        nombre = "Matias";
+        System.out.println("nombre = " + nombre);
+        
+    }
+
+    
+}
